02f05f030800ff518afc2bb992eee6be6d5d0d3e
Matthew McAchran <mattmcachran@gmail.com>
2024-01-29 11:50:14 -0500

    Create check-404s.sh

---
+++ scripts/check-404s.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+export LC_ALL=C
+
+# General variables.
+readonly WORK_DIR=$(dirname "$(readlink --canonicalize-existing "${0}" 2> /dev/null)")
+
+
+WEBSITE_URLS=(
+    "https://www.highpoint.edu/"
+    "https://www.highpoint.edu/familyweekend/event/nido-and-mariana-qubein-childrens-museum-tours-3/"
+    "https://www.highpoint.edu/familyweekend/event/art-gallery-friday/"
+)
+
+for url in "${WEBSITE_URLS[@]}"; do
+    if curl -s --head --request GET ${url} | grep "200 OK" > /dev/null; then
+        continue
+    fi
+
+    echo "URL is returning a 404:  ${url}"
+
+    echo "Flushing permalinks..."
+    wp rewrite flush --url=${url} --path=/var/www/wordpress/
+done
