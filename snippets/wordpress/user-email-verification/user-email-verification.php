b10893b3c130f374a4762557b10f57ae88160b56
Matt McAchran <mattmcachran@gmail.com>
2024-07-02 14:57:55 -0400

    Updates

--- snippets/wordpress/user-email-verification/user-email-verification.php
+++ snippets/wordpress/user-email-verification/user-email-verification.php
@@ -1,15 +1,24 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
+function appp_is_user_email_domain_valid(string $user_email): bool
+{
+    $blacklisted_domains_path = WPMU_PLUGIN_DIR . '/blacklisted-domains.txt';
+
+    // error_log(print_r([$blacklisted_domains_path, @file_get_contents($blacklisted_domains_path)], true));
+
+    if (!file_exists($blacklisted_domains_path)) {
+        return true;
+    }
+
+    return (strpos(@file_get_contents($blacklisted_domains_path), $user_email) === false);
+}
 
 \add_filter(
     'registration_errors',
     function ($errors, $sanitized_user_login, $user_email) {
-        $blacklisted_domains_path = WPMU_PLUGIN_DIR . '/blacklisted-domains.txt';
-
-        if (!file_exists($blacklisted_domains_path)) {
-            return $errors;
-        }
-
-        if (strpos(file_get_contents($blacklisted_domains_path), $user_email) !== false) {
+        if (!appp_is_user_email_domain_valid($user_email)) {
             $errors->add(
                 'unauthorized_email',
                 'Unauthorized email used for signup!'
@@ -24,23 +33,37 @@
 
 \add_action(
     'bp_signup_validate',
-    function () {
-        $blacklisted_domains_path = WPMU_PLUGIN_DIR . '/blacklisted-domains.txt';
-
-        error_log(print_r([$blacklisted_domains_path, @file_get_contents($blacklisted_domains_path)], true));
-
-        if (!file_exists($blacklisted_domains_path)) {
-            return;
-        }
-
+    function (): void {
         global $bp;
 
         $user_email = $bp->signup->email ?? '';
 
-        if (strpos(@file_get_contents($blacklisted_domains_path), $user_email) !== false) {
+        if (empty($user_email)) {
+            return;
+        }
+
+        if (!appp_is_user_email_domain_valid($user_email)) {
             $bp->signup->errors['signup_unauthorized_email'] = 'Please register with a valid email address.';
         }
     },
     PHP_INT_MAX,
     0
+);
+
+add_filter(
+    'bp_core_validate_user_signup',
+    function ($result) {
+        if (!appp_is_user_email_domain_valid($result['user_email'])) {
+            $result['errors']->add(
+                'unauthorized_email',
+                'Unauthorized email used for signup!'
+            );
+        }
+
+        error_log(print_r(['appp_is_user_email_domain_valid', $result], true));
+
+        return $result;
+    },
+    PHP_INT_MAX,
+    1
 );
