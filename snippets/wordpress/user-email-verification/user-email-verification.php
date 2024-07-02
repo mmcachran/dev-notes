73aa15be62aaab4cd7eb017a5c2f88bf6fb36a02
Matt McAchran <mattmcachran@gmail.com>
2024-07-02 12:47:01 -0400

    Updates

--- snippets/wordpress/user-email-verification/user-email-verification.php
+++ snippets/wordpress/user-email-verification/user-email-verification.php
@@ -1,18 +1,11 @@
-<?php
-
-declare(strict_types=1);
-
-declare(ticks=1);
-
-use \WP_Error;
-use function \add_filter;
+<?php declare(strict_types=1);
 
 \add_filter(
     'registration_errors',
     function ($errors, $sanitized_user_login, $user_email) {
-        $blacklisted_domains_path = basename(dirname(__FILE__)) . '/blacklisted-domains.txt';
+        $blacklisted_domains_path = WPMU_PLUGIN_DIR . '/blacklisted-domains.txt';
 
-        if ( ! file_exists($blacklisted_domains_path)) {
+        if (!file_exists($blacklisted_domains_path)) {
             return $errors;
         }
 
@@ -27,4 +20,27 @@ use function \add_filter;
     },
     PHP_INT_MAX,
     3
+);
+
+\add_action(
+    'bp_signup_validate',
+    function () {
+        $blacklisted_domains_path = WPMU_PLUGIN_DIR . '/blacklisted-domains.txt';
+
+        error_log(print_r([$blacklisted_domains_path, @file_get_contents($blacklisted_domains_path)], true));
+
+        if (!file_exists($blacklisted_domains_path)) {
+            return;
+        }
+
+        global $bp;
+
+        $user_email = $bp->signup->email ?? '';
+
+        if (strpos(@file_get_contents($blacklisted_domains_path), $user_email) !== false) {
+            $bp->signup->errors['signup_unauthorized_email'] = 'Please register with a valid email address.';
+        }
+    },
+    PHP_INT_MAX,
+    0
 );
