d5a81075b1c113d297185cebce73f069322261e0
Matt McAchran <mattmcachran@gmail.com>
2024-07-02 11:57:38 -0400

    Updates

--- snippets/wordpress/user-email-verification/user-email-verification.php
+++ snippets/wordpress/user-email-verification/user-email-verification.php
@@ -8,23 +8,23 @@ use \WP_Error;
 use function \add_filter;
 
 \add_filter(
-    'pre_user_email',
-    function (string $user_email): string {
+    'registration_errors',
+    function ($errors, $sanitized_user_login, $user_email) {
         $blacklisted_domains_path = basename(dirname(__FILE__)) . '/blacklisted-domains.txt';
 
         if ( ! file_exists($blacklisted_domains_path)) {
-            return $user_email;
+            return $errors;
         }
 
         if (strpos(file_get_contents($blacklisted_domains_path), $user_email) !== false) {
-            return new \WP_Error(
+            $errors->add(
                 'unauthorized_email',
                 'Unauthorized email used for signup!'
             );
         }
 
-        return $user_email;
+        return $errors;
     },
-    1,
-    PHP_INT_MAX
+    PHP_INT_MAX,
+    3
 );
